f76c59e879e6be27ce713a3a54e65ec19108e7a4
fememm12 <Femiemm8196@gmail.com>
2021-08-14 14:54:06 +0530

    Femi - Registrastion Models Included

---
+++ iLearn/iLearn/Models/Organization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iLearn.Models
+{
+    public partial class Organization
+    {
+        public Organization()
+        {
+            Coursemain = new HashSet<Coursemain>();
+            User = new HashSet<User>();
+        }
+
+        public int Orgid { get; set; }
+        public string Orgname { get; set; }
+        public string Orgaddress { get; set; }
+        public string Orglocality { get; set; }
+        public string Orgstate { get; set; }
+        public string Orgcountry { get; set; }
+        public string Orgpin { get; set; }
+        public bool? Isactive { get; set; }
+        public DateTime? Createddate { get; set; }
+
+        public ICollection<Coursemain> Coursemain { get; set; }
+        public ICollection<User> User { get; set; }
+    }
+}
---
+++ iLearn/iLearn/Models/Userpermission.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iLearn.Models
+{
+    public partial class Userpermission
+    {
+        public Userpermission()
+        {
+            User = new HashSet<User>();
+        }
+
+        public int Permissionid { get; set; }
+        public int? Userid { get; set; }
+        public int? Courseid { get; set; }
+        public string Userpermission1 { get; set; }
+
+        public Coursemain Course { get; set; }
+        public User UserNavigation { get; set; }
+        public ICollection<User> User { get; set; }
+    }
+}
